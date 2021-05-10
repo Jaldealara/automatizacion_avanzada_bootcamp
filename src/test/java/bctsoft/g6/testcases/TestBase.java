448929340e7363e68388dbf36467d039545b4f25
Jeremy.Aldea <jeremy.aldea@tsoftglobal.com>
2021-05-10 15:28:45 -0400

    arreglando el caxo2

--- src/test/java/bctsoft/g6/testcases/TestBase.java
+++ src/test/java/bctsoft/g6/testcases/TestBase.java
@@ -3,6 +3,7 @@ package bctsoft.g6.testcases;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 <<<<<<< HEAD
+<<<<<<< HEAD
 import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
@@ -14,6 +15,13 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.AfterClass;
 >>>>>>> 411e24dc8c8d24156ca259cbc92336f887d99a09
+=======
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+>>>>>>> ff6ba8a (testng completo, cases separados)
 
 import java.util.concurrent.TimeUnit;
 
@@ -22,10 +30,14 @@ public class TestBase {
     //atributo
     protected WebDriver driver;
 <<<<<<< HEAD
+<<<<<<< HEAD
     @Before
 =======
     @BeforeClass
 >>>>>>> 411e24dc8c8d24156ca259cbc92336f887d99a09
+=======
+    @BeforeClass
+>>>>>>> ff6ba8a (testng completo, cases separados)
     public void setUp(){
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
@@ -37,10 +49,14 @@ public class TestBase {
      */
 
 <<<<<<< HEAD
+<<<<<<< HEAD
     @After
 =======
     @AfterClass
 >>>>>>> 411e24dc8c8d24156ca259cbc92336f887d99a09
+=======
+    @AfterClass
+>>>>>>> ff6ba8a (testng completo, cases separados)
     public void close(){
         driver.close();
     }
